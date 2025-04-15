2f3ab11d7d2a13a211b57af0f1decdb7b2af5fb7
moh4b-z <mohammadsalim10000@gmail.com>
2025-04-15 11:47:44 -0300

    envio

---
+++ app/src/main/java/br/senai/sp/jandira/lionschoolapp/screens/components/CourseCard.kt
@@ -0,0 +1,16 @@
+package br.senai.sp.jandira.lionschoolapp.screens.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun CourseCard(
+
+) {
+}
+
+@Preview
+@Composable
+private fun SearchInputPreview() {
+    CourseCard()
+}
